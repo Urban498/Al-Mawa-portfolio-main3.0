e57a51f9c78511e13664ed11f59932c46d8332c6
shashantalmawa <urbanrural498@gmail.com>
2026-06-23 14:34:54 +0530

    fix referral

--- src/app/referral/page.tsx
+++ src/app/referral/page.tsx
@@ -10,7 +10,7 @@ export default function ReferralPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+    <div className="min-h-screen mt-20 bg-gradient-to-b from-gray-50 to-white">
       <ReferralFormModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
 
       {/* Hero Section */}
